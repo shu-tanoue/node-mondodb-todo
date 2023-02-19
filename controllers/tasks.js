9ac2b3ba308e7d7a667ba06ab2aa68dea71a55ac
Shu <stbook.syu@icloud.com>
2023-02-19 21:24:56 +0900

    create taskRoute

---
+++ controllers/tasks.js
@@ -0,0 +1,26 @@
+const getAllTasks = (req, res) => {
+  res.send("タスクをすべて取得しました");
+};
+
+const createTask = (req, res) => {
+  res.send("タスクを新規作成しました");
+};
+
+const getSingeTask = (req, res) => {
+  res.send("ある特定のタスクを所得しました");
+};
+const updateTask = (req, res) => {
+  res.send("ある特定のタスクを更新しました");
+};
+
+const deleteTask = (req, res) => {
+  res.send("ある特定のタスクを削除しました");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getSingeTask,
+  updateTask,
+  deleteTask,
+};
